cada8f117e81313e58f63d0c6a4d5abcf5122042
griffonknox <thompsonscott1180@gmail.com>
2022-08-02 10:59:55 -0500

    email

--- OnePoint_Followup_Report/main.py
+++ OnePoint_Followup_Report/main.py
@@ -1,22 +1,7 @@
-from OnePoint_Followup_Report.utils import queryFollow_Ups, filterFollow_Ups, saveFollow_Ups
+from OnePoint_Followup_Report.utils import Main
 import logging
 
 logging.basicConfig(filename="Error_Log.txt", filemode='a', level=logging.DEBUG)
-
-
-def Main():
-    # try:
-
-    df = queryFollow_Ups()
-    df = filterFollow_Ups(df)
-    save_url = saveFollow_Ups(df)
-    print(save_url)
-    print("finishedasdfasdfasdf")
-
-
-    # except:
-    #     # logging.exception("\n\n {}, Report Failed!".format(datetime.now()))
-    #     print("failed")
 
 if __name__ == "__main__":
     Main()
--- OnePoint_Followup_Report/utils.py
+++ OnePoint_Followup_Report/utils.py
@@ -4,6 +4,11 @@ import pandas as pd
 from datetime import date, timedelta, datetime
 import pathlib
 from os.path import join
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import smtplib
+import logging
 
 def sql_cnx():
     username = ScriptConfig.USERNAME
@@ -45,10 +50,64 @@ def filterFollow_Ups(df):
     return df_last_week
 
 
+def countFollow_Ups(df):
+
+    users = df["varEnteredBy"].unique()
+    results = "\n\nUsername: FollowUps Created"
+    for user in users:
+        sort = df[df["varEnteredBy"] == user]
+        results += "\n{}: {}".format(user, len(sort.index))
+
+    return results
+
+
 def saveFollow_Ups(dataframe):
 
     starting_path = pathlib.Path(__file__).parent.absolute()
     save_location = join(starting_path, ScriptConfig.SAVE_LOCATION)
-    file_name = join(save_location, "{}.csv".format(f'{datetime.today():%Y-%m-%d}'))
+    file_name = join(save_location, "FollowUp_Report_{}.csv".format(f'{datetime.today():%Y-%m-%d}'))
     dataframe.to_csv(file_name, index=False)
     return file_name
+
+def Email_Success(save_url, results):
+    # Create a multipart message
+    msg = MIMEMultipart()
+    body_part = MIMEText("Weekly OnePoint FollowUp Report for the previous week.\nFull report is attached, quick statistics displayed below.{}".format(results), 'plain')
+    msg['Subject'] = "Weekly OnePoint FollowUp Report {}".format(f'{datetime.today():%Y-%m-%d}')
+    # Add body to email
+    msg.attach(body_part)
+
+    # open and read the CSV file in binary
+    with open(save_url, 'rb') as file:
+        # Attach the file with filename to the email
+        msg.attach(MIMEApplication(file.read(), Name="FollowUp_Report_{}.csv".format(f'{datetime.today():%Y-%m-%d}')))
+
+    Email_Send(ScriptConfig, msg)
+
+def Email_Send(CONFIG, msg):
+    server = smtplib.SMTP('smtp.office365.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(CONFIG.SMTP_EMAIL, CONFIG.SMTP_PW)
+
+    # Convert the message to a string and send it
+    for em in CONFIG.EMAIL_LIST:
+        server.sendmail(CONFIG.SMTP_EMAIL, em, msg.as_string())
+    server.quit()
+
+
+def Main():
+
+    try:
+
+        df = queryFollow_Ups()
+        df = filterFollow_Ups(df)
+        save_url = saveFollow_Ups(df)
+
+        results = countFollow_Ups(df)
+
+        Email_Success(save_url, results)
+
+
+    except:
+        logging.exception("\n\n {}, Report Failed!".format(datetime.now()))
